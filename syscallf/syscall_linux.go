7f832286b6f1d48d290e7862b96a38f7a10386db
Karol Jakusz-Gostomski <karol.jakusz.gostomski@gmail.com>
2024-10-09 18:17:02 +0200

    integer-overflow - copyright

--- syscallf/syscall_linux.go
+++ syscallf/syscall_linux.go
@@ -1,6 +1,5 @@
-// Copyright 2009 The Go Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
+// Copyright 2009 The Go Authors.
+// based on golang's syscall.InotifyRmWatch
 
 package syscallf
 
