f596c656de543685f559e22f7f28cb0580ef1429
Karol Jakusz-Gostomski <karol.jakusz.gostomski@gmail.com>
2024-10-09 18:27:46 +0200

    integer-overflow - no error conversion

--- syscallf/rm_watch.go
+++ syscallf/rm_watch.go
@@ -4,30 +4,6 @@ package syscallf
 
 import "syscall"
 
-// Do the interface allocations only once for common
-// Errno values.
-var (
-	errEAGAIN error = syscall.EAGAIN
-	errEINVAL error = syscall.EINVAL
-	errENOENT error = syscall.ENOENT
-)
-
-// errnoErr returns common boxed Errno values, to prevent
-// allocations at runtime.
-func errnoErr(e syscall.Errno) error {
-	switch e {
-	case 0:
-		return nil
-	case syscall.EAGAIN:
-		return errEAGAIN
-	case syscall.EINVAL:
-		return errEINVAL
-	case syscall.ENOENT:
-		return errENOENT
-	}
-	return e
-}
-
 func InotifyRmWatch(fd int, watchdesc int) (int, error) {
 	var success int
 	var err error
@@ -35,7 +11,7 @@ func InotifyRmWatch(fd int, watchdesc int) (int, error) {
 	r0, _, e1 := syscall.RawSyscall(syscall.SYS_INOTIFY_RM_WATCH, uintptr(fd), uintptr(watchdesc), 0)
 	success = int(r0)
 	if e1 != 0 {
-		err = errnoErr(e1)
+		err = e1
 	}
 	return success, err
 }
